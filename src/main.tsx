8bd3ea307d94a3bf5c07f25348ff55b8104fcd2f
ouadoudii <omar.elouadoudi@gmail.com>
2026-08-28 20:50:07 +0200

    Deploy Cary directly without archive restore

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { LanguageProvider } from './i18n/LanguageContext';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
